fb5495fa639c1774363576a36fb4a88a85aa1131
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2024-02-28 15:11:08 +0530

    Implement create new poll function

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const API_URL = "http://localhost:8080/api"
 
 const categoryContainerElm = document.querySelector('#categories');
+const btnAskElm = document.getElementById('btnAsk');
+const inputElm = document.getElementById('input');
+const chkboxElm = document.getElementById('chkbox');
 
 loadAllCategories();
 
@@ -12,7 +15,7 @@ function loadAllCategories() {
                     const newLiElm = document.createElement('li');
                     categoryContainerElm.append(newLiElm);
                     newLiElm.innerHTML = `<div class="flex-grow-1 d-flex gap-1 align-items-center">
-                    <input class="form-check-input" type="checkbox">
+                    <input id="chkbox" class="form-check-input" type="checkbox" value="${category}">
                     <label class="flex-grow-1">${category}</label>
                 </div>`;
                 })
@@ -25,3 +28,33 @@ function loadAllCategories() {
     });
 }
 
+
+btnAskElm.addEventListener('click', () => {
+    const text = inputElm.value.trim();
+    if (text.trim().length === 0) {
+        inputElm.focus();
+        inputElm.select();
+        return;
+    }
+
+    fetch(`${API_URL}/polls/create`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ 
+            title: text,
+            categoryId: 5,
+            options: ["Option 1", "Option 2"]})
+    }).then(res => {
+        if (res.ok) {
+            inputElm.value = "";
+            inputElm.focus();
+            alert("Poll Saved!");
+        } else {
+            alert("Failed to add the poll");
+        }
+    }).catch(err => {
+        alert("Something went wrong, Try again later");
+    });
+});
